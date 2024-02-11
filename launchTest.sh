3c19f7642c4d237082aee988298ba847f8f29919
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2024-02-11 17:33:48 +0530

    Quick commit

--- launchTest.sh
+++ launchTest.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 git pull
 
-docker-compose -f dc-test.yaml down
+# docker-compose -f dc-test.yaml down
+# docker build . -t ftesti
+# docker-compose -f dc-test.yaml up -d
+
+docker stop ft
+docker rm ft
 docker build . -t ftesti
-docker-compose -f dc-test.yaml up -d
+docker run -d --name ft ftesti
